4c0951d74361d8b2631fda2a86127a94614e5a70
Hanor <hanor_13ctb@hotmail.com>
2018-12-17 14:51:03 -0200

    Angular Guide - Melhoria de código

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'home',
@@ -7,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void{}
 
+  eventAngularStyleGuide(): void {
+    window.open('https://angular.io/guide/styleguide', '_blank');
+  }
 }
